a34a608102c8580227883188880db0db8243a2bb
owen-d <ow.diehl@gmail.com>
2016-03-13 11:11:29 -0400

    ex1

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -7,6 +7,7 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
+
 for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
@@ -16,11 +17,12 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
+    coeff = (alpha / m);
+    h = (X * theta - y);
+    thetaOneTemp = theta(1) - coeff * sum(h);
+    thetaTwoTep = theta(2) - coeff * sum(h .* X(:,2));
+    theta(1) = thetaOneTemp;
+    theta(2) = thetaTwoTep;
 
 
     % ============================================================
